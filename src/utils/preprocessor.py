8a4b32b9d04d97d15cbce9b84099d1c60f0ee001
dwaqar <120402562+dwaqar@users.noreply.github.com>
2022-12-12 12:21:56 -0500

    Add files via upload

---
+++ src/utils/preprocessor.py
@@ -0,0 +1,31 @@
+import numpy as np
+#from scipy.misc import imread, imresize
+import cv2
+import matplotlib.pyplot as plt
+
+def preprocess_input(x, v2=True):
+    x = x.astype('float32')
+    x = x / 255.0
+    if v2:
+        x = x - 0.5
+        x = x * 2.0
+    return x
+
+
+def _imread(image_name):
+        #img = cv2.imread(image_name) # changing this
+        img = plt.imread(image_name) # now this
+        #return imread(image_name)
+        return img
+
+
+def _imresize(image_array, size):
+        #return imresize(image_array, size)
+        return cv2.resize(image_array, size)
+
+def to_categorical(integer_classes, num_classes=2):
+    integer_classes = np.asarray(integer_classes, dtype='int')
+    num_samples = integer_classes.shape[0]
+    categorical = np.zeros((num_samples, num_classes))
+    categorical[np.arange(num_samples), integer_classes] = 1
+    return categorical
